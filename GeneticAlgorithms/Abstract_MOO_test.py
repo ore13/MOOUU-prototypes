constraint calculation fixing

GeneticAlgorithms/Abstract_MOO_test.py
@@ -17,7 +17,7 @@ class AbstractPopIndividualTests:
 
         def constraints(x):
             return np.array([x[1] + 9 * x[0] - 6, -x[1] + 9 * x[0] - 1])
-
+        moo = AbstractMOEA(objectives, [(0, 1), (0, 1)], 2, constraints=constraints)
         a = AbstractPopIndividual([0.2, 1])
         assert a.fitness == 0
         assert np.all(np.isclose(a.d_vars, np.array([0.2, 1])))
@@ -25,19 +25,17 @@ class AbstractPopIndividualTests:
         b = AbstractPopIndividual([0.2, 1], objective_values=np.array([0.2, 10], dtype=float))
         assert b.is_constrained is False
         assert np.all(np.isclose(b.objective_values, [0.2, 10]))
-        c = AbstractPopIndividual([0.2, 1], constraints)
+        c = AbstractPopIndividual([0.2, 1], is_constrained=True)
         assert c.is_constrained
-        assert c.constraints == constraints
-        assert c.violates
-        assert np.isclose(c.total_constraint_violation, 3.4)
-        d = AbstractPopIndividual([0.60000001, 0.6], constraints)
-        assert d.violates is False
-        assert np.isclose(d.total_constraint_violation, 0)
-        population = [a, d]
-        moo = AbstractMOEA(objectives, [(0, 1), (0, 1)], 2)
+        d = AbstractPopIndividual([0.60000001, 0.6], is_constrained=True)
+        population = [a, c, d]
         moo.run_model(population)
         assert np.all(np.isclose(a.objective_values, [0.2, 10]))
         assert np.all(np.isclose(d.objective_values, [0.6, 8 / 3]))
+        assert np.isclose(c.total_constraint_violation, 3.4)
+        assert c.violates is True
+        assert np.isclose(d.total_constraint_violation, 0)
+        assert d.violates is False
 
     @staticmethod
     def test_update():
@@ -46,29 +44,30 @@ class AbstractPopIndividualTests:
 
         def constraints(x):
             return np.array([x[1] + 9 * x[0] - 6, -x[1] + 9 * x[0] - 1])
-
-        a = AbstractPopIndividual([0.3, 1], constraints)
+        moo = AbstractMOEA(objectives, [(0, 1), (0, 1)], 2, constraints=constraints)
+        a = AbstractPopIndividual([0.3, 1], is_constrained=True)
+        assert a.calculate_constraints == a.run_model
         a.d_vars = np.array([0.2, 1], dtype=float)
         a.update()
-        assert a.is_constrained
-        assert a.constraints == constraints
-        assert a.violates
+        assert a.calculate_constraints == a.run_model
+        moo.run_model(AbstractPopulation([a]))
         assert np.isclose(a.total_constraint_violation, 3.4)
+        assert a.violates is True
+        assert np.all(np.isclose(a.objective_values, [0.2, 10]))
         a.d_vars = np.array([0.6000001, 0.6])
         a.update()
+        moo.run_model(AbstractPopulation([a]))
         assert a.violates is False
         assert np.isclose(a.total_constraint_violation, 0)
+        assert np.all(np.isclose(a.objective_values, [0.6, 8 / 3]))
         b = AbstractPopIndividual([0.1, 1])
         b.d_vars = np.array([0.2, 1], dtype=float)
         b.update()
+        moo.run_model(AbstractPopulation([b]))
         assert b.fitness == 0
         assert np.all(np.isclose(b.d_vars, np.array([0.2, 1])))
         assert b.is_constrained is False
-        population = [a, b]
-        moo = AbstractMOEA(objectives, [(0, 1), (0, 1)], 2)
-        moo.run_model(population)
         assert np.all(np.isclose(b.objective_values, [0.2, 10]))
-        assert np.all(np.isclose(a.objective_values, [0.6, 8/3]))
 
     @staticmethod
     def test_dominates():
@@ -91,12 +90,12 @@ class AbstractPopIndividualTests:
         def constraints(x):
             return np.array([x[1] + 9 * x[0] - 6, -x[1] + 9 * x[0] - 1])
 
-        a = AbstractPopIndividual([0.6, 0.7], constraints)
-        b = AbstractPopIndividual([0.8, 2], constraints)
-        c = AbstractPopIndividual([0.2, 1], constraints)
-        d = AbstractPopIndividual([0.1, 3], constraints)
+        a = AbstractPopIndividual([0.6, 0.7], is_constrained=True)
+        b = AbstractPopIndividual([0.8, 2], is_constrained=True)
+        c = AbstractPopIndividual([0.2, 1], is_constrained=True)
+        d = AbstractPopIndividual([0.1, 3], is_constrained=True)
         population = [a, b, c, d]
-        moo = AbstractMOEA(objectives, (0, 5), 2)
+        moo = AbstractMOEA(objectives, (0, 5), 2, constraints=constraints)
         moo.run_model(population)
         assert a.dominates(b)
         assert a.dominates(c)

GeneticAlgorithms/Abstract_Moo.py
@@ -61,19 +61,30 @@ class AbstractMOEA:
         if type(population) != "numpy.ndarray":
             population = np.array(population)
         positions = np.where([individual.run_model for individual in population])[0]
+        constraint_positions = np.where([individual.calculate_constraints for individual in population])
+        #assert np.all(constraint_positions == positions)
         ensemble = np.atleast_2d(np.array([individual.d_vars for individual in population[positions]]))
         ensemble = ensemble.T
         if len(ensemble) > 0:
-            data = np.atleast_2d(self.objectives(ensemble))
-            data = data.T
-            for i, individual in enumerate(population[positions]):
-                individual.objective_values = data[i]
+            objectives = np.atleast_2d(self.objectives(ensemble)).T
+            if self.is_constrained:
+                constraints = np.atleast_2d(self.constraints(ensemble)).T
+                constraints[np.where(constraints >= 0)] = 0
+                total_constraint_violation = np.sum(np.abs(constraints), axis=1)
+                for i, individual in enumerate(population[positions]):
+                    individual.objective_values = objectives[i]
+                    individual.total_constraint_violation = total_constraint_violation[i]
+                    individual.violates = bool(individual.total_constraint_violation > 0)
+            else:
+                for i, individual in enumerate(population[positions]):
+                    individual.objective_values = objectives[i]
+
 
     def run_model_IO(self, population, decision_variable_template='individual_{}.inp',
                   objective_template='individual_{}.out'):
         input_files = population.write_decision_variables(decision_variable_template)
         output_files = [objective_template.format(i + 1) for i in range(len(input_files))]
-        for I, O in zip(input_files, output_files):  # TODO: substitute this for parallel run use pyemu.helpers.setup_slaves (or something)
+        for I, O in zip(input_files, output_files):
             cmd = self.model_path + [self.model, '--input_file', I, '--output_file', O]
             subprocess.run(cmd, shell=True)
         population.read_objectives(objective_template)
@@ -223,7 +234,7 @@ class AbstractPopulation:
         if model_update is False:
             positions = np.arange(0, len(self))
         else:
-            positions = np.where([individual.calculate_objectives for individual in self.population])[0]
+            positions = np.where([individual.run_model for individual in self.population])[0]
         return np.atleast_2d(np.array([individual.d_vars for individual in self.population[positions]]))
 
     def write_decision_variables(self, filename_template):
@@ -240,7 +251,7 @@ class AbstractPopulation:
         return file_names
 
     def read_objectives(self, filename_template):
-        positions = np.where([individual.calculate_objectives for individual in self.population])[0]
+        positions = np.where([individual.run_model for individual in self.population])[0]
         file_names = [filename_template.format(i) for i in range(1, len(positions) + 1)]
         for filename, individual in zip(file_names, self.population[positions]):
             df = pd.read_csv(filename, header=0, index_col=0, encoding='ascii').T
@@ -263,12 +274,12 @@ class AbstractPopulation:
 class AbstractPopIndividual:
     """represents an individual in a population for NSGA-II"""
 
-    def __init__(self, d_vars, constraints=None, objective_values=None, total_constraint_violation=None):
+    def __init__(self, d_vars, is_constrained=False, objective_values=None, total_constraint_violation=None):
         """initialise the new population member"""
         self.d_vars = np.array(d_vars, dtype=float)
         # self.objectives = objectives
         self.fitness = 0
-        self.is_constrained = not (constraints is None)
+        self.is_constrained = is_constrained
         self.run_model = False
         self.calculate_constraints = False
         if objective_values is None:
@@ -277,17 +288,20 @@ class AbstractPopIndividual:
         else:
             self.objective_values = objective_values
         if self.is_constrained:
-            self.constraints = constraints
             if total_constraint_violation is None:
-                self.total_constraint_violation = self.calculate_constrained_values(self.constraints, self.d_vars)
                 self.calculate_constraints = True
+                self.violates = None
             else:
                 self.total_constraint_violation = total_constraint_violation
+                self.violates = bool(self.total_constraint_violation > 0)
                 self.calculate_constraints = False
-            self.violates = bool(self.total_constraint_violation > 0)
 
     def update(self):
+        self.objective_values = None
+        self.total_constraint_violation = None
+        self.violates = None
         self.run_model = True
+        self.calculate_constraints = True
 
     def __str__(self):
         try:

TestProblems/StochasticProblemSuite.py
@@ -49,6 +49,10 @@ class BenchmarkTestProblem:
         raise Exception("Child class should define this function")
 
     @staticmethod
+    def constrained():
+        return False
+
+    @staticmethod
     def _quadratic_positive_root(a, b, c):
         return (-b + np.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
 
@@ -352,7 +356,8 @@ class CONSTR(DeterministicBenchmark):
     def __str__(self):
         return "CONSTR"
 
-    def constrained(self):
+    @staticmethod
+    def constrained():
         return True
 
     @staticmethod
@@ -409,7 +414,8 @@ class SRN(DeterministicBenchmark):
     def __str__(self):
         return "SRN"
 
-    def constrained(self):
+    @staticmethod
+    def constrained():
         return True
 
     @staticmethod
@@ -440,11 +446,11 @@ class SRN(DeterministicBenchmark):
 
     @staticmethod
     def calculate_objectives(d_vars, pars):
-        return np.array(SRN.f1(d_vars), SRN.f2(d_vars))
+        return np.array([SRN.f1(d_vars), SRN.f2(d_vars)])
 
     @staticmethod
     def calculate_constraints(d_vars, pars):
-        return np.array(SRN.constraint1(d_vars), SRN.constraint2(d_vars))
+        return np.array([SRN.constraint1(d_vars), SRN.constraint2(d_vars)])
 
     def get_pareto_front(self):
         maximiser1 = -2.5
@@ -529,8 +535,11 @@ class IOWrapper:
         model = test_functions[args.benchmark_function.lower()]
         d_vars, pars = self.read_input_file(args.input_file, model)
         objectives = model.calculate_objectives(d_vars, pars)
-        constraints = model.calculate_constraints(d_vars, pars)
-        self.write_output_file(objectives, constraints, args.output_file)
+        if model.constrained():
+            constraints = model.calculate_constraints(d_vars, pars)
+        else:
+            constraints = []
+        self.write_output_file(objectives=objectives, constraints=constraints, output_file=args.output_file)
 
     @staticmethod
     def parse():
@@ -569,7 +578,7 @@ class IOWrapper:
         return d_vars, pars
 
     @staticmethod
-    def write_output_file(objectives, constraints, output_file):
+    def write_output_file(objectives, output_file, constraints=None):
         num_objectives = len(objectives)
         num_constraints = len(constraints)
         data = np.concatenate((objectives, constraints))